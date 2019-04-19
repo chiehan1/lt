8e95311e0b18327bd93bbd17d79258d0f597e5d4
chiehan1 <chieh.an.hsu@gmail.com>
2019-04-19 11:06:21 +0800

    add code

--- integrate-garchen-csv/main.js
+++ integrate-garchen-csv/main.js
@@ -1,24 +1,42 @@
-import { promises as fsPromise }, fs from 'fs';
+import { readFile, writeFileSync } from 'fs';
+import Path from 'path';
 import glob from 'glob';
 import { toObject as csv2json } from 'csvjson';
 import { compare as tibetanSort } from 'tibetan-sort-js';
 import { union, asyncWrap } from './helpers';
-import tibColName from './tibColName';
+import { fnTibColMap, tibCnDictionaryName } from './tibColName';
+
+let getTibEntry = tibEntryColumn => entryObj => {
+  let tibEntry = entryObj[tibEntryColumn];
+  return tibEntry.replace(/^[་། ]*(.+?)[་། ]*$/, '$1') + '་';
+};
+
+const checkTibEntry = entry => {
+  return /^[\u0f00-\u0fff ()]+$/.test(entry);
+};
 
 const integrateGarchenCsv = async () => {
   const globRoute = './dictionaryCsvs/**/*.csv';
   const csvRoutes = await asyncWrap(glob)(globRoute);
 
-  const boEntryArrs = await Promise.all(csvRoutes.map(route => {
-    const csv = await fsPromise.readFile(route, 'utf8');
-    const json = csv2json(csv);
+  const boEntryArrs = await Promise.all(csvRoutes.map(async route => {
+    const csv = await asyncWrap(readFile)(route, 'utf8');
+    const entryObjs = csv2json(csv);
 
+    const isTibCnDictionary = (new RegExp(tibCnDictionaryName)).test(route);
+    const dictionaryName =  isTibCnDictionary ? tibCnDictionaryName : Path.basename(route, '.csv');
+    const tibEntryColumn = fnTibColMap[dictionaryName];
+
+    const getTibEntryByColName = getTibEnty(tibEntryColumn);
+    return entryObjs.map(getTibEntryByColName).filter(checkTibEntry);
   }));
 
   const boEntries = union(...boEntryArrs).sort(tibetanSort)
     .map(entry => { bo: entry });
 
-  fs.writeFile('./result.js', `export default = ${JSON.stringify(boEntries, null, '  ')}`, 'utf8');
+  const jsonString = JSON.stringify(boEntries, null, '  ');
+  const resultString = `export default = ${jsonString};`;
+  writeFileSync('./result.js', resultString, 'utf8');
 };
 
 integrateGarchenCsv();
