42f267417f94796a9e805588590ae95490cbe26b
chiehan1 <chieh.an.hsu@gmail.com>
2019-05-10 14:31:16 +0800

    add lhasakangyur to get-stacks

--- get-stacks/main.js
+++ get-stacks/main.js
@@ -5,21 +5,22 @@ import naturalSort from 'javascript-natural-sort';
 
 const fields = [
   { label: '疊字', value: 'syl' },
-  { label: '麗江甘珠爾', value: 'jiangkangyur' },
-  { label: '德格甘珠爾', value: 'degekangyur' },
-  { label: '德格丹珠爾', value: 'degetengyur' },
-  { label: '岡波巴大師全集', value: 'gampopa' },
-  { label: '第八世法王全集', value: '8thkarmapa' },
-  { label: '局米旁大師全集', value: 'mipam' },
-  { label: '宗喀巴大師全集', value: 'tsongkhapa' },
-  { label: '果然巴大師全集', value: 'gorampa' },
-  { label: '多羅那他大師全集', value: 'taranatha' },
-  { label: '苯教藏經', value: 'bonpokangyur' }
+  { label: '麗江甘珠爾\njiangkangyur', value: 'jiangkangyur' },
+  { label: '德格甘珠爾\ndegekangyur', value: 'degekangyur' },
+  { label: '拉薩甘珠爾\nlhasakangyur', value: 'lhasakangyur' },
+  { label: '德格丹珠爾\ndegetengyur', value: 'degetengyur' },
+  { label: '岡波巴大師全集\ngampopa', value: 'gampopa' },
+  { label: '第八世法王全集\n8thkarmapa', value: '8thkarmapa' },
+  { label: '局米旁大師全集\nmipam', value: 'mipam' },
+  { label: '宗喀巴大師全集\ntsongkhapa', value: 'tsongkhapa' },
+  { label: '果然巴大師全集\ngorampa', value: 'gorampa' },
+  { label: '多羅那他大師全集\ntaranatha', value: 'taranatha' },
+  { label: '苯教藏經\nbonpo', value: 'bonpokangyur' }
 ];
 
 const json2csv = new Parser({ fields });
 
-const dbNames = [ 'jiangkangyur', 'degekangyur', 'degetengyur', 'gampopa', '8thkarmapa', 'mipam', 'tsongkhapa', 'gorampa', 'taranatha', 'bonpokangyur'];
+const dbNames = [ 'jiangkangyur', 'degekangyur', 'lhasakangyur', 'degetengyur', 'gampopa', '8thkarmapa', 'mipam', 'tsongkhapa', 'gorampa', 'taranatha', 'bonpokangyur'];
 
 const hasStack = (str) => {
   return /[\u0f71\u0f84\u0f8d-\u0f92\u0f94-\u0f9c\u0f9e-\u0fa1\u0fa3-\u0fa6\u0fa8-\u0fab\u0fae-\u0fb0\u0fb3-\u0fb8\u0fba-\u0fbc]{2,}/.test(str.normalize('NFKD'));
